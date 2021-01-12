d5b963af4d326ffaea6ac41305de4b2dfbd51660
Zeynep Canbulat <72697219+zeynepcanbulat@users.noreply.github.com>
2021-01-13 01:24:19 +0300

    Update alıştırmalar4_1.py

--- alıştırmalar4_1.py
+++ alıştırmalar4_1.py
@@ -1,17 +1,25 @@
 from math import sqrt
 liste = []
 def sezarın_kutusu():
-    a = input ("Cümleyi giriniz: ")
+    string = input ("Cümleyi giriniz: ")
+    a = list(string)
     x = len(a)
+    
     while sqrt(x) != round(sqrt(x)):
         x = x+1
-    print (sqrt(x))
+
+    print(x)
+    
+    fark = x - len(a) #n*n matris olmadığında problem yaşamayalım diye
+    print(fark)
+    for k in range(0,fark):
+            a.append(" ")
+    print(a)
+    
     for i in range (0,round(sqrt(x))):
         for n in range(0,round(sqrt(x))):
             if n != round(sqrt(x)):
                 kripto = a[i + round(sqrt(x))*n]
             liste.append(kripto)
     print(liste)
-                
-                
-            
+liste = []
